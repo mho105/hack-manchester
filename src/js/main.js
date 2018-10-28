91ac866b7365e43982d6c09a8384340cc7c86e26
Mark Holden <maholden@gmail.com>
2018-10-28 01:50:19 +0100

    Shake

--- src/js/main.js
+++ src/js/main.js
@@ -17,13 +17,48 @@ function init() {
 function detectMovement() {
     window.addEventListener("devicemotion", handleMotion, true);
 
-    function handleMotion(event) {
-        var r = event.accelerationIncludingGravity;
-        console.log(event);
-        if(r.x !== null && r.y !== null && r.z !== null) {
-            console.log('has moved');
-            var element = document.getElementById('device');
-            element.classList.add('cross');
+    var lastX, lastY, lastZ;
+    var moveCounter = 0;
+
+    function handleMotion(e) {
+        let acc = e.acceleration;
+        if(!acc.hasOwnProperty('x')) {
+            acc = e.accelerationIncludingGravity;
+        }
+    
+        if(!acc.x) return;
+    
+        //only log if x,y,z > 1
+        if(Math.abs(acc.x) >= 1 &&
+        Math.abs(acc.y) >= 1 &&
+        Math.abs(acc.z) >=1) {
+            //console.log('motion', acc);
+            if(!lastX) {
+                lastX = acc.x;
+                lastY = acc.y;
+                lastZ = acc.z;
+                return;
+            }
+    
+            let deltaX = Math.abs(acc.x - lastX);
+            let deltaY = Math.abs(acc.y - lastY);
+            let deltaZ = Math.abs(acc.z - lastZ);
+            
+            if(deltaX + deltaY + deltaZ > 3) {
+                moveCounter++;
+            } else {
+                moveCounter = Math.max(0, --moveCounter);
+            }
+    
+            if(moveCounter > 2) {
+                console.log('SHAKE!!!');
+                moveCounter = 0;
+            }
+    
+            lastX = acc.x;
+            lastY = acc.y;
+            lastZ = acc.z;
+            
         }
     }
 }
